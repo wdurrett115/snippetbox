16e50ff8bc350531470fc64a9a28b4620005153e
William Durrett <durrettwilliam54@gmail.com>
2024-10-20 19:08:15 -0500

    Chapter 4.8 addtions, note home function has commented code

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,6 +13,16 @@ import (
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
+	// snippets, err := app.snippets.Latest()
+	// if err != nil {
+	// 	app.serverError(w, r, err)
+	// 	return
+	// }
+
+	// for _, snippet := range snippets {
+	// 	fmt.Fprintf(w, "%+v\n", snippet)
+	// }
+
 	files := []string{
 		"./ui/html/base.tmpl.html",
 		"./ui/html/partials/nav.tmpl.html",
