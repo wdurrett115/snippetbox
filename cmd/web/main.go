511258619fe5be4f97b3ac80038fb8061eab07cd
William Durrett <durrettwilliam54@gmail.com>
2024-11-12 18:25:00 -0600

    Chapter 7.6 - added go-playground form decoder to simplify form handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,7 @@ import (
 
 	"w.durrett.mod/internal/models"
 
+	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -18,6 +19,7 @@ type application struct {
 	logger        *slog.Logger
 	snippets      *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder   *form.Decoder
 }
 
 func main() {
@@ -40,10 +42,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	formDecoder := form.NewDecoder()
+
 	app := &application{
 		logger:        logger,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecoder:   formDecoder,
 	}
 
 	logger.Info("starting server", "addr", *addr)
