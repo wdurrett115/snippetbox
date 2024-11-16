59c5b43387db07b31c5447cd4a17bba8683e9894
wdurrett115 <durrettwilliam54@gmail.com>
2024-11-16 14:47:44 -0600

    Chapter 9.2 - Moved server errors to custom logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -61,8 +61,9 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:    *addr,
-		Handler: app.routes(),
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
 	logger.Info("starting server", "addr", *addr)
