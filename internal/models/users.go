091efaa6ac09e2a57144b66c72843d747c20f233
William Durrett <durrettwilliam54@gmail.com>
2024-11-25 21:03:49 -0600

    Fix SQL statement error

--- internal/models/users.go
+++ internal/models/users.go
@@ -48,7 +48,7 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 	var id int
 	var hashedPassword []byte
 
-	stmt := "SELECT id, hased_password FROM users WHERE email= ?"
+	stmt := "SELECT id, hashed_password FROM users WHERE email= ?"
 
 	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
 	if err != nil {
