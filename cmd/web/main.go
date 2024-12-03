dbaf71c49f745a923f5745032b2f28d3d64addcd
William Durrett <durrettwilliam54@gmail.com>
2024-12-02 20:24:30 -0600

    Chapter 12 - added embedded filesystem to serve static files and templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,7 +8,6 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
-	"path/filepath"
 	"time"
 
 	"w.durrett.mod/internal/models"
@@ -98,34 +97,4 @@ func openDB(dsn string) (*sql.DB, error) {
 	}
 
 	return db, nil
-}
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := f.Stat()
-	if err != nil {
-		return nil, err
-	}
-	if s.IsDir() {
-		index := filepath.Join(path, "index.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			closeErr := f.Close()
-			if closeErr != nil {
-				return nil, closeErr
-			}
-
-			return nil, err
-
-		}
-	}
-
-	return f, nil
 }
